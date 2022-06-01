9e952cb43adf46152074d3cc17992c223a4c1705
Daniel Tovar <daniel@danieltovar.io>
2022-05-31 20:39:03 -0500

    feat: fetcher util

---
+++ lib/fetcher.ts
@@ -0,0 +1,13 @@
+export default async function fetcher(url: string, data = undefined) {
+  const res = await fetch(`${window.location.origin}/api${url}`, {
+    method: data ? 'POST' : 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  if (res.status > 399 && res.status < 200) {
+    throw new Error()
+  }
+  return res.json()
+}
