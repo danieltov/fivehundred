b193b57a83a4b8a905ab3982460802fb28c05d61
Daniel Tovar <daniel@danieltovar.io>
2022-06-19 12:55:57 -0500

    feat: add Footer with placerholder text

---
+++ components/Footer.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Grid, GridItem, Heading, Link, Text } from '@chakra-ui/layout'
+import NextLink from 'next/link'
+
+export const Footer = () => {
+  return (
+    <Grid as="footer" padding="28" templateColumns="repeat(2, 1fr)">
+      <GridItem>
+        <Heading as="h3" size="lg">
+          A project from{' '}
+          <NextLink href="https://danieltovar.io" passHref>
+            <Link isExternal fontWeight="bolder">
+              Daniel Tovar
+            </Link>
+          </NextLink>
+          {'. '}
+        </Heading>
+      </GridItem>
+      <GridItem>
+        <Heading as="h3" size="lg">
+          About
+        </Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo enim fugit corrupti, officiis quisquam rerum
+          consectetur repudiandae quidem quaerat nisi dolorum est libero amet pariatur.
+        </Text>
+      </GridItem>
+    </Grid>
+  )
+}
