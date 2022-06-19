29b17da12f64c693bb6fec65d5d6438376a85b18
Daniel Tovar <daniel@danieltovar.io>
2022-06-19 13:01:48 -0500

    feat: replace tier with isAPlus - migration - api route changes - update hooks

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,12 +3,9 @@ import { AlbumsResponse, DetailsResponse } from '../@types/api'
 import { Album, DetailWithAlbums } from '../@types/ui'
 import fetcher from './fetcher'
 
-// TODO - tier albums
-// TODO const A_PLUS_TIER = 'a-plus-tier'
-
 /* Shelf View Hooks */
 
-// TODO export const useAPlus = () => useQuery<Album[]>('a-plus', () => fetcher(`/get/tier/${A_PLUS_TIER}`))
+export const useAPlus = () => useQuery<Album[]>('a-plus', () => fetcher(`/get/album/a-plus`))
 export const useAllAlbums = () => useQuery<AlbumsResponse>('all', () => fetcher(`/get/album`))
 export const useDetails = (type: string) => useQuery<DetailsResponse>(type, () => fetcher(`/get/${type}`))
 
