3ea0c4739a56aa1a17e8961cb1b143d001ac1b59
Daniel Tovar <daniel@danieltovar.io>
2022-06-21 21:53:00 -0500

    ci: update migrations

---
+++ prisma/migrations/20220622025110_add_slugs/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Album" ADD COLUMN "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "Artist" ADD COLUMN "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "Descriptor" ADD COLUMN "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "Genre" ADD COLUMN "slug" TEXT;
