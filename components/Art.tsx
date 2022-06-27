a3373b8632bca5bf82a2451495780181b47aeeaf
Daniel Tovar <daniel@danieltovar.io>
2022-06-26 20:27:54 -0500

    refactor(Art): mobile style

--- components/Art.tsx
+++ components/Art.tsx
@@ -11,25 +11,27 @@ export const Art = ({ coverArt }: Props) => {
   return (
     <Box
       as="picture"
-      width={['85vw', '500px']}
-      height={['85vw', '500px']}
-      _after={{
-        ...(!isMobile && {
-          content: `""`,
-          position: 'absolute',
-          bg: 'gray.800',
-          borderRadius: '100%',
-          height: '90%',
-          width: '90%',
-          top: '5%',
-          left: '15%',
-          zIndex: 0,
-          transitionDuration: '0.5s',
-        }),
-      }}
+      width={['100%', '500px']}
+      height={['100%', '500px']}
+      _after={
+        isMobile
+          ? null
+          : {
+              content: `""`,
+              position: 'absolute',
+              bg: 'gray.800',
+              borderRadius: '100%',
+              height: '90%',
+              width: '90%',
+              top: '5%',
+              left: '15%',
+              zIndex: 0,
+              transitionDuration: '0.5s',
+            }
+      }
       _hover={{
         _after: {
-          left: ['15%', '25%'],
+          left: '25%',
         },
       }}
       display="inline-block"
