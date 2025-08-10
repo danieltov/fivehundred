deb60058e41ac56b826b31782b4d45780e301496
Daniel Tovar <daniel@danieltovar.io>
2025-08-09 20:18:53 -0500

    feat(prisma): sanitize album title and fetch Spotify URI if missing 🎶

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,4 +1,5 @@
 import { useQuery } from 'react-query'
+
 import { AlbumsResponse, DetailsResponse } from '../@types/api'
 import { Album, DetailWithAlbums } from '../@types/ui'
 import fetcher from './fetcher'
@@ -7,9 +8,11 @@ import fetcher from './fetcher'
 
 export const useAPlus = () => useQuery<Album[]>('a-plus', () => fetcher(`/get/album/a-plus`))
 export const useAllAlbums = () => useQuery<AlbumsResponse>('all', () => fetcher(`/get/album`))
-export const useDetails = (type: string) => useQuery<DetailsResponse>(type, () => fetcher(`/get/${type}`))
+export const useDetails = (type: string) =>
+  useQuery<DetailsResponse>(type, () => fetcher(`/get/${type}`))
 
 /* Summary View Hooks */
-export const useSingleAlbum = (albumId: string) => useQuery<Album>('album', () => fetcher(`/get/album/${albumId}`))
+export const useSingleAlbum = (albumId: string) =>
+  useQuery<Album>('album', () => fetcher(`/get/album/${albumId}`))
 export const useSingleDetail = (type: string, detailId: string) =>
   useQuery<DetailWithAlbums>(type, () => fetcher(`/get/${type}/${detailId}`))
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,46 @@
 import { PrismaClient } from '@prisma/client'
 
-export default new PrismaClient()
+import { searchSpotifyAlbum } from './music-apis'
+
+// Simple HTML entity decoder for common entities
+function decodeHtmlEntities(str: string): string {
+  if (!str) return str
+  return str
+    .replace(/&amp;/g, '&')
+    .replace(/&quot;/g, '"')
+    .replace(/&apos;/g, "'")
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&#39;/g, "'")
+    .replace(/&#34;/g, '"')
+    .replace(/&#38;/g, '&')
+    .replace(/&#60;/g, '<')
+    .replace(/&#62;/g, '>')
+}
+
+const prisma = new PrismaClient()
+
+prisma.$use(async (params, next) => {
+  // Only intercept album.create
+  if (params.model === 'Album' && params.action === 'create') {
+    // Sanitize title
+    if (params.args.data && params.args.data.title) {
+      params.args.data.title = decodeHtmlEntities(params.args.data.title)
+    }
+    // If spotifyUri is missing, try to fetch it
+    if (!params.args.data.spotifyUri && params.args.data.artist && Array.isArray(params.args.data.artist.connect) && params.args.data.artist.connect.length > 0) {
+      // Fetch artist name from DB
+      const artistId = params.args.data.artist.connect[0].id
+      const artist = await prisma.artist.findUnique({ where: { id: artistId } })
+      if (artist) {
+        const spotifyUri = await searchSpotifyAlbum(artist.name, params.args.data.title)
+        if (spotifyUri) {
+          params.args.data.spotifyUri = spotifyUri
+        }
+      }
+    }
+  }
+  return next(params)
+})
+
+export default prisma
